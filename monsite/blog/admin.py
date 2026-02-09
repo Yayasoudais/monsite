a81d15072650c41f6e90852612f376f70bfc1df1
Mamadou Yaya Diallo <mamadouyaya.soudais34@gmail.com>
2026-02-10 00:21:39 +0100

    La mise en place de la classe Article dans le models, et la personnalisation du fichier admin

--- monsite/blog/admin.py
+++ monsite/blog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+@admin.register(Article) 
+class ArticleAdmin(admin.ModelAdmin): 
+    """Configuration de l"interface admin pour Article"""
+    
+    # Champs affichés dans la liste 
+    
+    list_display = ('titre', 'date_publication', 'publie')
+    # Champs filtrables 
+    
+    list_filter = ('publie', 'date_publication')
+    
+    # Barre de recherche 
+    
+    search_fields = ('titre', 'contenu')
+    
+    # Champs remplis automatiquement 
+    
+    prepopulated_fields = {'slug': ('titre',)}
+    
+    # Ordre des champs dans le formulaire 
+    
+    fields = ('titre', 'slug', 'contenu', 'date_publication', 'publie')
+    
+    # Ou grouper par sections 
+
+    # fieldsets = ( 
+    #   ('Informations principales', {
+    #     'fields': ('titre', 'slug', 'contenu')
+    #   }),
+    #   ('Paramètres de publication', {
+    #    'fields': ('date_publication', 'publie'')
+    #     }),
--- monsite/blog/models.py
+++ monsite/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Article(models.Model):
+    """"Model représentant un article de blog"""
+    
+    titre = models.CharField(max_length=200, verbose_name="Titre") 
+    slug = models.SlugField(max_length=200, unique=True) 
+    contenu = models.TextField(verbose_name="Contenu") 
+    date_creation = models.DateTimeField(auto_now_add=True) 
+    date_modification = models.DateTimeField(auto_now=True) 
+    date_publication = models.DateTimeField(default=timezone.now) 
+    publie = models.BooleanField(default=False, verbose_name="publié")
+    
+    class Meta:
+        verbose_name = "Article" 
+        verbose_name_plural = "Articles" 
+        ordering = ['-date_publication'] # Plus récent en premier
+        
+    def __str__(self):
+        """Reprêsentatior textuelle de l'objet""" 
+        return self.titre
