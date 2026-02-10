823ac610084c423d0e46a71e92a86e4df3b476f4
Mamadou Yaya Diallo <mamadouyaya.soudais34@gmail.com>
2026-02-10 13:57:18 +0100

    Mise en place de la classe catégorie dans le model et modification du fichier admin

--- monsite/blog/admin.py
+++ monsite/blog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Categorie
 
 # Register your models here.
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('nom','slug')
+    search_fields = ('nom',)
+    prepopulated_fields = {'slug':('nom',)}
 
 @admin.register(Article) 
 class ArticleAdmin(admin.ModelAdmin): 
@@ -9,14 +15,15 @@ class ArticleAdmin(admin.ModelAdmin):
     
     # Champs affichés dans la liste 
     
-    list_display = ('titre', 'date_publication', 'publie')
+    list_display = ('titre','categorie','date_publication','publie')
+    
     # Champs filtrables 
     
-    list_filter = ('publie', 'date_publication')
+    list_filter = ('publie','date_publication','categorie')
     
     # Barre de recherche 
     
-    search_fields = ('titre', 'contenu')
+    search_fields = ('titre','contenu')
     
     # Champs remplis automatiquement 
     
@@ -24,7 +31,7 @@ class ArticleAdmin(admin.ModelAdmin):
     
     # Ordre des champs dans le formulaire 
     
-    fields = ('titre', 'slug', 'contenu', 'date_publication', 'publie')
+    fields = ('titre', 'slug', 'contenu','categorie', 'date_publication', 'publie')
     
     # Ou grouper par sections 
 
--- monsite/blog/models.py
+++ monsite/blog/models.py
@@ -3,12 +3,39 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Categorie(models.Model): 
+    """Catégories pour organiser les articles"""
+    
+    nom = models.CharField(max_length=100, unique=True) 
+    slug = models.SlugField(max_length=100, unique=True) 
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Catégorie" 
+        verbose_name_plural = "Catégories" 
+        ordering = ['nom']
+    
+    def __str__(self): 
+        return self.nom
+
+
 class Article(models.Model):
     """"Model représentant un article de blog"""
     
     titre = models.CharField(max_length=200, verbose_name="Titre") 
     slug = models.SlugField(max_length=200, unique=True) 
     contenu = models.TextField(verbose_name="Contenu") 
+    
+    # NOUVEAU : Relation avec Catégorie (on verra ça demain en détail) 
+    
+    categorie = models.ForeignKey( 
+        Categorie, 
+        on_delete = models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name='articles',
+    )
+    
     date_creation = models.DateTimeField(auto_now_add=True) 
     date_modification = models.DateTimeField(auto_now=True) 
     date_publication = models.DateTimeField(default=timezone.now) 
@@ -20,5 +47,5 @@ class Article(models.Model):
         ordering = ['-date_publication'] # Plus récent en premier
         
     def __str__(self):
-        """Reprêsentatior textuelle de l'objet""" 
+        """Représentation textuelle de l'objet""" 
         return self.titre
